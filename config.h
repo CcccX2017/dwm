49474f6602fa9c95ad09a95efd0114cadee032c1
WackyChen <nebulapsns@gmail.com>
2022-04-27 21:17:23 +0800

    fix font

--- config.h
+++ config.h
@@ -5,10 +5,10 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "JetBrain Mono Nerd Font:size=16",
+static const char *fonts[]          = { "JetBrainsMono Nerd Font:size=16",
 				        "JoyPixels:size=16"
 				      };
-static const char dmenufont[]       = "JetBrain Mono Nerd Font:size=16";
+static const char dmenufont[]       = "JetBrainMono Nerd Font:size=16";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
