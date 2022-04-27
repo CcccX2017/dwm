ed2e5be3227a1c4acb233a86c376267c4e90dc4b
WackyChen <nebulapsns@gmail.com>
2022-04-27 22:46:46 +0800

    apply patch -> dwm-rotatetags-20210723-cb3f58a.diff

--- dwm.c
+++ dwm.c
@@ -206,6 +206,7 @@ static void resizeclient(Client *c, int x, int y, int w, int h);
 static void resizemouse(const Arg *arg);
 static void restack(Monitor *m);
 static void rotatestack(const Arg *arg);
+static void rotatetags(const Arg *arg);
 static void run(void);
 static void runAutostart(void);
 static void scan(void);
@@ -1650,6 +1651,33 @@ rotatestack(const Arg *arg)
 		restack(selmon);
 	}
 }
+
+void
+rotatetags(const Arg *arg)
+{
+	const int rot = abs(arg->i);
+	const unsigned int tagset = selmon->tagset[selmon->seltags];
+	unsigned int newtagset;
+
+	/* check the direction of the shift */
+	if (arg->i < 0) {
+		 /* shift tags right */
+		 newtagset = (tagset >> rot) | (tagset << (LENGTH(tags) - rot));
+	} else {
+		 /* shift tags left */
+		 newtagset = (tagset << rot) | (tagset >> (LENGTH(tags) - rot));
+	}
+
+	/* mask the tag bits */
+	newtagset &= TAGMASK;
+
+	if (newtagset) {
+		 selmon->tagset[selmon->seltags] = newtagset;
+		 focus(NULL);
+		 arrange(selmon);
+	}
+}
+
 
 void
 run(void)
