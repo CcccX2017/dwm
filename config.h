04a2708179bdab212a6f82e49746cf19ef4d3734
WackyChen <nebulapsns@gmail.com>
2022-04-27 22:21:36 +0800

    apply patch -> dwm-viewontag-20210312-61bb8b2.diff

--- config.h
+++ config.h
@@ -6,6 +6,7 @@ static const unsigned int gappx     = 8;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
+static const Bool viewontag	    = True;     /* Switch View on tag switch */
 static const char *fonts[]          = { "JetBrainsMono Nerd Font:size=16",
 				        "JoyPixels:size=16"
 				      };
